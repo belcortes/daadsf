426c1a097bc2d02ffea676696d3e5f3fb58ad881
Isabel Cortes <belcortes@gmail.com>
2014-04-08 11:21:34 -0700

    first commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CEAD::Application.config.session_store :cookie_store, key: '_CEAD_session'
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+CEAD::Application.routes.draw do
+  devise_for :users
+  
+  root 'home#index'
+
+end

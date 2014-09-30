9ad272db4b0ac062189f15b356dd8c85aa9d32bf
Isabel Cortes <belcortes@gmail.com>
2014-09-30 11:29:37 -0700

    login changes, nothing fixed

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,19 @@
 class RegistrationsController < Devise::RegistrationsController
+  # respond_to :json
+  # def create
+  #   params[:user][:password_confirmation] = params[:user][:password]
+  #   respond_to :json
+  #   super
+  # end
 
   def create
-    build_resource
- 
+    build_resource(sign_up_params)
+
     if resource.save
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
         sign_up(resource_name, resource)
-        return render :json => {:success => true}
+        return render 'signup.js.erb'
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
         expire_session_data_after_sign_in!

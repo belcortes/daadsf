7c1c6531dde361ab36db450a8f306c98feb7f5e8
Isabel Cortes <belcortes@gmail.com>
2014-10-23 15:31:56 -0700

    login to view website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # require 'twitter'
 
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with :name => "daad", :password => "4theC!ty"
   # Prevent CSRF attacks by raising an exception.
   # For APIs you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -16,6 +17,10 @@ class ApplicationController < ActionController::Base
   #     config.access_token_secret = 'LicGYPVWd43oMvAt5CBacrZfYgtgp06Xzq7LUNKq0GJWG'
   #   end
   # end
+
+  def login_onload
+
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :nert, :cert, :birth, :phone, :name, :zip, :address, :cpr, :safe, :firstresponder, :firstaid, :dumptruck, :backhoe, :crane, :forklift, :truck, :fuel, :generator, :jackhammer, :emergency, :drill, :hammer, :shovel, :city, :state, :engineer, :contractor, :church, :school, :underground, :overnight, :oxygen, :wheelchair, :defibrillation, :other, :firstreponder, :inspector, :commercial, :electrician, :carpenter, :operator, :plumber, :kitchen, :bulldozer, :excavator, :frontloader, :reclaimer, :trencher, :tractor, :goggles, :axe, :address2, :backhoe_q, :bulldozer_q, :dumptruck_q, :excavator_q, :forklift_q, :frontloader_q, :jackhammer_q, :reclaimer_q, :tractor_q, :trencher_q, :axe_q, :drill_q, :fuel_q, :goggles_q, :hammer_q, :shovel_q, :defibrillation_q, :emergency_q, :oxygen_q, :wheelchair_q, :generator_q, :kitchen_q, :overnight_q, :underground_q, :business, :ngo, :gov, :faith, :closed, :open, :rescue, :flashlight, :flashlight_q, :individual) }

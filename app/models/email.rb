f2b59127be18b525677d7a24f56c4616d474dbda
Isabel Cortes <belcortes@gmail.com>
2015-01-23 11:23:21 -0800

    ...

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,7 +1,7 @@
 class Email < ActiveRecord::Base
-  has_attached_file :item, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
-  :path => ":rails_root/public/assets/photos/:id/:style/:basename.:extension", 
-  :url  => "/assets/photos/:id/:style/:basename.:extension"
+  has_attached_file :item, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  # :path => ":rails_root/public/assets/photos/:id/:style/:basename.:extension", 
+  # :url  => "/assets/photos/:id/:style/:basename.:extension"
   validates_attachment_content_type :item, :content_type => /\Aimage\/.*\Z/
   validates_attachment_presence :item
 

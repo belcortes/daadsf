c98d06ecb502113c3d3e5eb06689b3353c63205d
Isabel Cortes <belcortes@gmail.com>
2015-01-26 11:08:43 -0800

    reverting commits

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -31,3 +31,41 @@ ActiveAdmin.register_page "Dashboard" do
     # end
   end # content
 end
+
+# ActiveAdmin.register Group, as: 'Group Leader' do
+#   permit_params :email, :name, :admin_user_ids => []
+#   # scope_to :current_admin_user, :if => proc{ current_admin_user.leader? }  
+#   scope_to :current_admin_user, :unless => proc{ current_admin_user.super? }  
+#   menu :if => proc{ !current_admin_user.leader?}
+
+#   index do
+#     # current_admin_user.groups.each do |g|
+#     selectable_column
+#     id_column
+#     column :name
+#     column :email
+#     column :sign_in_count
+#     column :created_at
+#     actions
+#     # end
+#   end
+
+#   filter :email
+#   filter :name
+#   filter :sign_in_count
+#   filter :created_at
+
+#   form do |f|
+#     f.inputs "Admin Details" do
+#       f.input :name
+#       f.input :email,                   :label => "Email Address"
+#       if f.object.id.nil?
+#         f.input :password,              :label => "Password"
+#         f.input :password_confirmation, :label => "Password Confirmation"
+#       end
+#       f.input :users, :as => :check_boxes, :collection => User.all
+#     end
+#     f.actions
+#   end
+
+# end

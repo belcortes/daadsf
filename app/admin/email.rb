1b3e0417e80ae244c834c3d4cdbb64b70474b0e2
Isabel Cortes <belcortes@gmail.com>
2015-02-01 18:42:15 -0800

    photo tab fixes

--- app/admin/email.rb
+++ app/admin/email.rb
@@ -1,18 +1,21 @@
 ActiveAdmin.register Email, as: 'Images' do
   # before_filter :skip_sidebar!
+  menu priority: 1
   permit_params :subject, :from, :text
   actions :all, :except => [:new]
 
-  action_item only: :index do
-    link_to 'Publish'
-  end
+  # action_item only: :index do
+  #   link_to 'Publish'
+  # end
 
   index do
     selectable_column
+    column :published
     column "Images" do |e|
       image_tag("https://s3-us-west-1.amazonaws.com/sfdaad-production/emails/items/000/000/0#{e.item.instance.id}/original/#{e.item_file_name}" )
     end
     column :from
+    column :subject
   end
 
   batch_action :publish do |selection|
@@ -23,15 +26,13 @@ ActiveAdmin.register Email, as: 'Images' do
     redirect_to :back
   end
 
-  controller do
-    def publish_images(image)
-      @image = image
-      p @image
-      respond_to do |format|
-        format.json { render json: @image }
-        # format.html
-      end
+  batch_action :unpublish do |selection|
+    Email.find(selection).each do |e|
+      p e.item.path
+      e.update_attributes(published: false)
     end
+    redirect_to :back
   end
+
 
 end

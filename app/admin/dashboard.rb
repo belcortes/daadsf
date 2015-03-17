1e8666cbc5ba7065d77344bd9ed26d0ea106f972
Isabel Cortes <belcortes@gmail.com>
2015-03-17 10:48:50 -0700

    drawing tools

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -11,45 +11,43 @@ ActiveAdmin.register_page "Dashboard" do
 	# 	small I18n.t("active_admin.dashboard_welcome.call_to_action")
 	#   end
 	# end
-
-	# Here is an example of a simple dashboard with columns and panels.
 	
 	columns do
 	  column do
-	    panel "Recent Users" do
+	    panel link_to "Recent Users", admin_users_path do
 	      # ul do
-        User.all.map do |post|
-          li link_to(post.name, admin_admin_users_path)
+        User.limit(20).order('id desc').map do |post|
+          li link_to(post.name, admin_user_path(post.id))
         end
 	      # end
 	    end
 	  end
 
 	  column do
-	    panel "Recent Incidents" do
+	    panel link_to "Recent Incidents", admin_incidents_path do
 	      ul do
-	        Incident.where(r_type: 'incident').map do |post|
-	          li link_to(post.description, admin_admin_users_path)
+	        Incident.where(r_type: 'incident').limit(5).order('id desc').map do |post|
+	          li link_to(post.description, admin_incident_path(post.id))
 	        end
 	      end
 	    end
-	    panel "Recent Assets" do
+	    panel link_to "Recent Assets", admin_assets_path do
 	      ul do
-	        Incident.where(r_type: 'asset').map do |post|
-	          li link_to(post.description, admin_admin_users_path)
+	        Incident.where(r_type: 'asset').limit(5).order('id desc').map do |post|
+	          li link_to(post.description, admin_asset_path(post.id))
 	        end
 	      end
 	    end
-	    panel "Recent Resources" do
+	    panel link_to "Recent Resources", admin_resources_path do
 	      ul do
-	        Incident.where(r_type: 'resource').map do |post|
-	          li link_to(post.description, admin_admin_users_path)
+	        Incident.where(r_type: 'resource').limit(5).order('id desc').map do |post|
+	          li link_to(post.description, admin_resource_path(post.id))
 	        end
 	      end
 	    end
 	  end
 	  column do
-	    panel "Recent Images" do
+	    panel link_to "Recent Images", admin_images_path do
 	      # ul do
         Email.all.map do |post|
           li image_tag("https://s3-us-west-1.amazonaws.com/sfdaad-production/emails/items/000/000/0#{post.item.instance.id}/original/#{post.item_file_name}" )
